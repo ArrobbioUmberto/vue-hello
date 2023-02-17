58ee119b753496db37f00675f0d6d52118d7a88a
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-17 13:31:53 +0100

    FEAT data parameters

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 const { createApp } = Vue
-    
-      createApp({
-        data() {
-          return {
-            message: 'Hello Vue!'
-          }
-        }
-      }).mount('#app')
 
-console.log('ciao')
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            image: './img/boolean_logo.png',
+            alt_image: 'immagine messa da js'
+        }
+    }
+}).mount('#app')
+
+
+// TEST per un nuovo div 
+
+// createApp({
+//     data(){
+//         return{
+//             titolo: "Sono un nuovo testo su un nuovo div",
+//             text: "vediamo se faccio casino",
+//             stile: 'bg-primary text-center p-2'
+//         }
+//     }
+// }).mount('.prova')
+
+// console.log('ciao')
